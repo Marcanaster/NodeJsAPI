f9ade513990373eac3b20ba5895a5f1af0a52487
Marcos Almeida <marcanaster@gmail.com>
2022-11-18 16:40:21 -0300

    API metodo POST

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
-'use stricts';
+"use stricts";
 
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
 const router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 var route = router.get("/", (req, res, next) => {
-    res.status(200).send({
-      title: `API rodando na porta ${port}`,
-      version: "0.0.2",
-    });
+  res.status(200).send({
+    title: `API rodando na porta ${port}`,
+    version: "0.0.2",
   });
+});
 
-  app.use("/", route);
+const create = router.post("/", (req, res, next) => {
+  res.status(201).send(req.body);
+});
 
-  module.exports = app;
+app.use("/", route);
+app.use("/products", route);
+
+module.exports = app;
