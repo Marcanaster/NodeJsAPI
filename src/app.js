3a24c3db9fd24cc5a3e1fa7dbba861928bee3a95
Marcos Almeida <marcanaster@gmail.com>
2022-11-19 11:24:47 -0300

    mongoose

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 
 const express = require("express");
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+mongoose.connect('mongodb+srv://marcanaster:481521@cluster0.ba9caoz.mongodb.net/?retryWrites=true&w=majority');
 
 const indexRoute = require('./routes/index');
 const productRoute = require('./routes/products');
